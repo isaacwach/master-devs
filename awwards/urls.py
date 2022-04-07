0ab67ebffe9f0f94a085fc86af08f8972f6703a5
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-08 00:10:51 +0300

    added paths to project urls

--- awwards/urls.py
+++ awwards/urls.py
@@ -18,4 +18,13 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('',include('awwards.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, 
+        document_root=settings.MEDIA_ROOT
+    )
