0fc8b8e0a8ed3736a2cdbbf6fc64da77a1523666
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-08 17:27:36 +0300

    added all the necessary importations in the views

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, redirect, get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as django_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
+from .models import *
+from .forms import *
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+# from rest_framework import viewsets
+# from rest_framework import permissions
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
 #home view to render projects on the index page
 def index(request):
