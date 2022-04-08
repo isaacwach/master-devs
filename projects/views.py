bc6151a6fba84966aaf510d2904c2c4a797ea1d6
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-08 14:26:41 +0300

    created index and profile views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+#home view to render projects on the index page
+def index(request):
+    projects = Project.get_projects()
+    context={
+        'projects' : projects,
+    }
+    return render(request,"index.html", context)
+
+#profile view to query users by id and display them
+@login_required(login_url='/accounts/login/')
+def profile(request,profile_id):
+    profile = Profile.objects.get(pk = profile_id)
+    project = Project.objects.filter(profile_id=profile).all()
+    
+    context = {
+        'profile':profile,
+        'project':project
+    }
+    return render(request,"profile.html", context)
