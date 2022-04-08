33a3b6de4f0150e3997f481947cb4c7e857dc4ff
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-08 16:56:34 +0300

    created new_profile view

--- projects/views.py
+++ projects/views.py
@@ -20,6 +20,7 @@ def profile(request,profile_id):
     }
     return render(request,"profile.html", context)
 
+#profile view funcion with a for loopto loop through all profiles and display them by id
 @login_required(login_url='/accounts/login/')
 def project(request):
     current_user = request.user
@@ -42,3 +43,28 @@ def project(request):
                 'form':form
             }
             return render(request,'project.html', context)
+
+        @login_required(login_url='/accounts/login/')
+def new_profile(request):
+    project = Project.objects.filter(author=request.user).order_by('-date_posted')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'project' : project
+    }
+    return render(request, 'profile/profile.html', context)
