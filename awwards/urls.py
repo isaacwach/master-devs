7a0390fcc77261ca674d03f9d5f0487751e1fab7
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-04-10 20:19:17 +0300

    added database details

--- awwards/urls.py
+++ awwards/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('',include('awwards.urls')),
+    path('',include('projects.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
 if settings.DEBUG:
